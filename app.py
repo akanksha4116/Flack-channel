27be544cb59716ccdbf991c40980842ec042204b
akanksha <akanksha4116@gmail.com>
2020-05-29 12:35:12 +0530

    deploying

---
+++ app.py
@@ -0,0 +1,156 @@
+from flask import Flask, jsonify, render_template, request, url_for, send_from_directory
+from flask_socketio import SocketIO, send, emit
+
+app=Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    TEMPLATES_AUTO_RELOAD =True,
+    EXPLAIN_TEMPLATE_LOADING=True
+)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+socketio=SocketIO(app)
+channels={
+    'general':[]
+}
+users={
+    'general':[]
+
+}
+messages=[]
+typing_users={
+    'general':[]
+}
+
+def get_channels():
+    res=[]
+    for keys in channels.keys():
+        res.append(keys)
+        return res
+
+def append_message(list,msg):
+ if len(list) == 100:
+  list.pop(0)
+ else:
+  list.append(msg)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/checkuser", methods=["POST"])
+def checkuser():
+    UserName=request.form.get("username")
+    usernames={"admin"}
+    print(users.keys())
+    for key,value in users.items():
+        for user in value:
+            usernames.add(user)
+        print(usernames)    
+    if UserName in usernames:
+        return jsonify({"exists":True})
+    else:
+        return jsonify({"exists":False})
+
+@socketio.on('send_message')
+def handle_message(msg):
+    if "connection" in msg  and not msg["username"]==None and msg["username"] not in users[msg["channel"]]:
+        message={
+            "connection":True,
+            "text":"connected",
+            "username": msg['username'],
+			"date": msg['date'],
+			"channel": msg['channel'],
+			"files": {}
+        }
+        
+        users[msg["channel"]].append(msg["username"])
+        append_message(channels[msg["channel"]],message)
+        print(channels[msg['channel']])
+        emit('announce_message', {'messages': channels[msg['channel']]}, broadcast=True)
+        	
+    elif "connection" in msg and msg['username'] in users[msg['channel']]:
+        
+	    emit('announce_message', {'messages': channels[msg['channel']]})
+    elif "connection" not in msg:
+        print("1")
+        message = {
+			"connection": False,
+			"text": msg['text'],
+			"username": msg['username'],
+			"date": msg['date'],
+			"channel": msg['channel']
+			
+		}
+        append_message(channels[msg['channel']], message)
+        print(channels[msg['channel']])
+        emit('announce_message', message, broadcast=True)
+
+@socketio.on("change_username")
+def change_username(msg):
+    old_username=msg['old_username']
+    new_username=msg['new_username']
+    
+    for channel in channels.keys():
+        for n,msg in enumerate(channels[channel]):
+            if(msg['username']==old_username):
+             channels[channel][n]["username"] = new_username
+    
+    for channel in users.keys():
+        for n,msg in enumerate(users[channel]):
+            message = {
+					"connection": True,
+					"text": f"is now {new_username}",
+					"username": old_username,
+				#	"date": msg['date'],
+					'channel': channel
+				}
+            append_message(channels[channel], message)
+            users[channel][n] = new_username
+        print(channels[channel]) 
+        emit('announce_message', {'messages': channels[channel]},broadcast=True)
+
+
+@socketio.on('add_channel')
+def add_channel(msg):
+    if(msg['channel'] not in channels):
+      channels[msg['channel']]=[]
+      users[msg['channel']]=[]
+      print(channels)
+      emit('added_channel',{'message' : msg['channel']},broadcast=True)
+
+@socketio.on('get all channels')
+def get_all_channels(msg):
+	emit('all_channels', {'channels': get_channels()})
+
+@socketio.on("type")
+def on_type(msg):
+	username = msg['username']
+	channel = msg['channel']
+	if msg['status'] == "end":
+		if msg['username'] in typing_users[msg['channel']]:
+			typing_users[msg['channel']].remove(msg['username'])
+
+		message = {
+			"usernames": typing_users[channel],
+			"channel": channel,
+			"files": {}
+		}
+
+		emit('typing', message, broadcast=True)
+	else:
+		if channel not in typing_users:
+			typing_users[channel] = []
+		if username not in typing_users[channel]:
+			typing_users[channel].append(username)
+
+		message = {
+			"usernames": typing_users[channel],
+			"channel": channel,
+			"files": {}
+		}
+
+		emit('typing', message, broadcast=True)
+
+
+if __name__ == '__main__':
+	socketio.run(app)
---
+++ static/index.js
@@ -0,0 +1,381 @@
+$(document).ready(function(){
+   $("#username-form").modal('show');
+   var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    function get_date(){
+      
+        var date =new Date();
+
+            var day=date.getDate();
+            var month =months[date.getMonth()];
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            if (minutes / 10 < 1) {
+                return `${day} ${month}  ${hours}:0${minutes}`;
+            } else {
+                 return `${day} ${month}  ${hours}:${minutes}`;
+            }
+    }
+   
+    
+    //Testing purpose
+    localStorage.clear();
+
+    // set local storage for current channel
+    if (!localStorage.getItem('currentChannel')) {
+        localStorage.setItem('currentChannel', "general");
+    }
+    
+    
+    if(!localStorage.getItem('username')||localStorage.getItem('username') === ""){
+        $('#username-form').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    } else {
+        $("#username-btn").html(localStorage.getItem('username'));
+    }
+    
+    //To Change Username
+
+    $("#username-btn").on('click', function () {
+       
+
+        $("#change-username-form").modal('show');
+    });
+
+    $("#username-btn").hover(function(){
+        $(this).html('Change')
+
+    },function () {
+        $(this).html(localStorage.getItem('username'));
+    });
+
+
+   
+   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    $("#start").on('click',function(){
+        console.log("click");
+      
+      if($("#username-input").prop('value')==="")
+       {
+        $(".username-alert").css("display", "block");
+       }
+      else{
+          var date= get_date();
+                   const  request=new XMLHttpRequest();
+          request.open("POST","/checkuser")
+           
+          request.onload = () => {
+          const data=    JSON.parse(request.responseText);
+          
+         if(!data.exists)
+         {  //username don't exists
+         
+         localStorage.setItem('username',$("#username-input").val())
+         
+         $("#username-btn").html(localStorage.getItem('username'));
+
+         $("#username-form").modal('hide');
+
+         socket.emit('send_message', {
+            "connection": true,
+            "channel": localStorage.getItem('currentChannel'),
+            "username": localStorage.getItem('username'),
+            "date": date
+           });
+
+         }
+        else // username exits
+        {
+            $(".user-exists-alert").css("display", "block");
+        }
+    }//request.onload() close tag
+
+    //Add data to send
+      data=new FormData();
+      data.append('username',$("#username-input").val())
+      //Request send for checking user
+      request.send(data)
+      }
+        
+
+
+
+   });
+
+   $("#change-btn").on('click', function () {
+    if ($("#change-username-input").prop("value") === "") {
+        $(".change-username-alert").css("display", "block");
+    } else {
+        // Initialize new request
+        const request = new XMLHttpRequest();
+        request.open('POST', '/checkuser');
+
+        // Callback function for when request completes
+        request.onload = () => {
+
+            // Extract JSON data from request
+            const data = JSON.parse(request.responseText);
+
+            // Update the result div
+            if (!data.exists) {
+                let old_username = localStorage.getItem('username');
+                $(".change-username-alert").css("display", "none");
+                localStorage.setItem('username', $("#change-username-input").val());
+                $("#username-btn").html(localStorage.getItem('username'));
+                $('#change-username-form').modal('hide');
+
+               
+
+                socket.emit('change_username', {
+                    "connection": false,
+                    "channel": localStorage.getItem('currentChannel'),
+                    "new_username": localStorage.getItem('username'),
+                    "old_username": old_username,
+                    "date": date
+                });
+
+            } else {
+                $(".change-user-exists-alert").css("display", "block");
+            }
+        }
+
+        // Add data to send with request
+        const data = new FormData();
+        data.append('username', $("#change-username-input").val());
+
+        // Send request
+        request.send(data);
+    }
+});
+
+   
+    $("#emojionearea1").emojioneArea({
+        
+            tonesStyle: "bullet"
+    });
+
+    
+   
+     //done
+    $(".send-btn").on('click',function(){
+        
+        var date= get_date();
+        
+       message= $("#emojionearea1").val();
+       if(message=="")
+       {return}
+       else
+       { 
+       $(".emojionearea-editor").html('')
+       
+        socket.emit('send_message', {
+            "username": localStorage.getItem('username'),
+            "text": message,
+            "date": date,
+            "channel": localStorage.getItem('currentChannel'),
+            
+        });
+        
+       }
+        
+    });
+
+    //on pressing enter for  sending msg
+
+   
+    //adding new channel
+    $('.add-public-channel').on('click',function(){
+        if ($("#public-channel-input").val() == "") {
+            return;
+        }
+       let channel= $("#public-channel-input").val()
+       $("#public-channel-input").val("");
+       socket.emit('add_channel',{
+           'channel':channel
+       });
+
+    });
+   //click on any channel
+   $("#public-channels").on('click', 'h6', function (e) {
+    if (!$(this).hasClass("channel-active")) {
+        $("#typingUsersText").html('');
+        $(".typingUsers").css("display", "none");
+        $(".channel-active").removeClass("channel-active");
+        $(this).addClass("channel-active");
+        localStorage.setItem('currentChannel', $(this).html());
+
+        let date = get_date();
+        socket.emit('send_message', {
+            "connection": true,
+            "channel": localStorage.getItem("currentChannel"),
+            "username": localStorage.getItem('username'),
+            "date": date
+        });
+
+        if ($(".menu-btn").css("display") != "none") {
+            $('.menu').toggleClass('menu_active');
+            $('.content').toggleClass('content_active');
+        }
+    }
+
+    
+   // e.preventDefault();
+});
+
+
+    socket.on('connect',function(){
+        var date= get_date();
+        
+        socket.emit('send message', {
+            "connection": true,
+            "channel": localStorage.getItem("currentChannel"),
+            "username": localStorage.getItem('username'),
+            "date": date
+        });
+
+        
+            
+        socket.emit('get all channels', 
+        {"username": localStorage.getItem('username')});
+
+
+
+    });
+
+    socket.on('announce_message',function(msg){
+         
+        if(msg['messages'])
+        {   
+            
+            if(msg['messages'][0] && msg['messages'][0]['channel'] == localStorage.getItem('currentChannel'))
+            {
+                $("#messages").html("");
+            }
+             
+            messages=msg['messages']
+            messages.forEach(msg_ => {
+                
+            if(msg_['channel']==localStorage.getItem('currentChannel'))
+              {  
+                  //connected 1st time
+                
+                if(msg_['connection']){
+
+                    $("#messages").append(`<div class="row msg-row justify-content-center"><div class="msg-connect">
+                    <p><b>${msg_['username']}</b> ${msg_['text']}<span class="message-date">${msg_['date']}</span></p></div></div>`)
+                }
+                else{
+                    if (msg_['username'] == localStorage.getItem('username')) {
+                        $("#messages").append(`<div class="row msg-row justify-content-end"><div class="message-wrapper">
+                                            <p><b>${msg_['username']}</b><span class="message-date">${msg_['date']}</span></p>${msg_['text']}</div></div>`);
+                    } else {
+                        
+                        $("#messages").append(`<div class="row msg-row"><div class="message-wrapper msg-other">
+                                            <p><b>${msg_['username']}</b><span class="message-date">${msg_['date']}</span></p>${msg_['text']}</div></div>`);
+                    }
+                }
+
+            }
+            });
+
+        } //for chat
+        else {
+              
+            if (msg['username'] == localStorage.getItem('username')) {
+                $("#messages").append(`<div class="row msg-row justify-content-end"><div class="message-wrapper">
+                                                <p><b>${msg['username']}</b><span class="message-date">${msg['date']}</span></p>${msg['text']}</div></div>`);
+            } else {
+                $("#messages").append(`<div class="row msg-row"><div class="message-wrapper msg-other">
+                                                <p><b>${msg['username']}</b><span class="message-date">${msg['date']}</span></p>${msg['text']}</div></div>`);
+            }
+
+        }
+
+    })
+
+    socket.on('added_channel',function(msg){
+     
+       
+        if (msg['message'] !== "general") {
+            $("#public-channels").append(`<h6 class="channel">${msg['message']}</h6>`);
+        }
+     
+
+    });
+
+    socket.on('all_channels',function(channels){
+         $("#public-channels").html(`<h6 class="channel">general</h6>`);
+
+         channels['channels'].forEach(function (channel) {
+                if (channel !== "general") {
+                    $("#public-channels").append(`<h6 class="channel">${channel}</h6>`);
+                }
+          });
+
+          $("h6").each(function () {
+            if ($(this).html() == localStorage.getItem('currentChannel')) {
+                $(this).addClass("channel-active");
+            }
+        });
+    });
+
+     socket.on('typing',function(msg){
+         if(msg['channel']==localStorage.getItem('currentChannel')){
+            if (msg['usernames'].length == 0 || msg['usernames'][0] == localStorage.getItem('username') && msg['usernames'].length == 1) {
+                $("#typingUsersText").html('');
+                $(".typingUsers").css("display", "none");
+            } else {
+                $("#typingUsersText").html('');
+                
+                msg['usernames'].forEach(function (username) {
+                    if (username == localStorage.getItem('username')) {
+                        $("#typingUsersText").append(`${username}, `);
+                    }
+
+                    let temp = $("#typingUsersText").html().slice(0, -2);
+                    
+                    $("#typingUsersText").html(temp);
+                    if (msg['usernames'].length == 1 || msg['usernames'].length == 2 && msg['usernames'].includes(localStorage.getItem('username'))) {
+                        $("#typingUsersText").append(" is typing");
+                    } else {
+                        $("#typingUsersText").append(" are typing");
+                    }
+                    $(".typingUsers").css("display", "block");
+
+                });
+
+            }
+
+
+         }
+
+
+     });
+
+    //
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault();
+        $('.menu').toggleClass('menu_active');
+        $('.content').toggleClass('content_active');
+    });
+     //for displaying who is typing
+
+     $('#message').bind('input propertychange', function () {
+        socket.emit('type', {
+            "status": "start",
+            "channel": localStorage.getItem('currentChannel'),
+            "username": localStorage.getItem('username')
+        });
+
+    });
+
+    $("#message").blur(function () {
+        socket.emit('type', {
+            "status": "end",
+            "channel": localStorage.getItem('currentChannel'),
+            "username": localStorage.getItem('username')
+        });
+    });
+
+});
